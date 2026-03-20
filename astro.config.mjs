5f97de1ddca5b26e5878b0d2d74564bf3ac524a2
1ut0 <108579396+1ut0@users.noreply.github.com>
2026-03-20 15:06:30 -0300

    Corrección de para el despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://lemon-beach-056777310.6.azurestaticapps.net',
+    output: 'static',
     vite: {
         plugins: [tailwindcss()],
     },
